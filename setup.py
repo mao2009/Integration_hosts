76e06feba2f3534801f0163ac870fa15cdff2d24
mao2009 <entrprise@gmail.com>
2019-09-23 16:06:55 +0900

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='hostsfile',
     install_requires=["requests"],
-    version='1.0.1',
+    version='1.0.2',
     packages=['hostsfile'],
     url='',
     license='BSD',
